2840354287793fc144efccba42f8b96b7c9cdc7e
yuriminion <yuriming112@gmail.com>
2024-04-03 23:07:11 +0900

    1.5) 자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,28 @@
+// 1. Number Type
+
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let minf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = "박유림";
+let myLocation = "성남";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다.`; // 템플릿 리터럴 문법
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
